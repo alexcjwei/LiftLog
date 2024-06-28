cc155e73bf0de718e1a2c541a6551d71285df292
alexcjwei <alexcj.wei@gmail.com>
2024-06-28 16:56:14 -0700

    adds update view for workout, lets users update name of workout

--- backend/workouts/urls.py
+++ backend/workouts/urls.py
@@ -30,12 +30,10 @@ workout_exercise_patterns = [
     ),
 ]
 set_patterns = [
-    path("", views.WorkoutListView.as_view(), name="index"),
-    path("create/", views.WorkoutCreateView.as_view(), name="create"),
-    path("<int:pk>/", views.WorkoutDetailView.as_view(), name="detail"),
-    path("<int:pk>/update", views.WorkoutUpdateView.as_view(), name="update"),
-    path("<int:pk>/copy", views.workout_copy, name="copy"),
-    path("<int:pk>/delete", views.workout_delete, name="delete"),
+    path("exercises/<int:workout_exercise_id>/add-set/", views.set_add, name="set_add"),
+    path("sets/<int:pk>/edit/", views.SetUpdateView.as_view(), name="set_update"),
+    path("sets/<int:pk>/delete/", views.set_delete, name="set_delete"),
+    path("sets/<int:pk>/copy/", views.set_copy, name="set_copy"),
 ]
 
 urlpatterns = workout_patterns + workout_exercise_patterns + set_patterns
