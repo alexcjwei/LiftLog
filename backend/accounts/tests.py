4008fc318bff2fb950f0506a832ef5b7316f5677
alexcjwei <alexcj.wei@gmail.com>
2024-07-01 19:59:47 -0700

    adds test that user cannot be registered with duplicate email

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from accounts import models
 
-# Create your tests here.
+
+class SignupViewTests(TestCase):
+    def test_signup_no_duplicate_email(self):
+        # Create a user with the same email as the one we're going to try to sign up with
+        User.objects.create_user(
+            username="testuser", email="test@test.com", password="testpassword"
+        )
+        response = self.client.post(
+            reverse("accounts:signup"),
+            {
+                "username": "testuser2",
+                "email": "test@test.com",
+                "password": "testpassword",
+            },
+        )
+        self.assertContains(response, "A user with that email already exists.")
+        self.assertFalse(User.objects.filter(username="testuser2").exists())
+        self.assertFalse(
+            models.Profile.objects.filter(user__username="testuser2").exists()
+        )
