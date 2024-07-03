22ed7ab97a7e5f2978e1a24620a2cc9587a7d8c5
alexcjwei <alexcj.wei@gmail.com>
2024-07-03 11:33:38 -0700

    adds follows to profile model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+        "self", related_name="followed_by", symmetrical=False
+    )
 
     class Meta:
         db_table = "profiles"
