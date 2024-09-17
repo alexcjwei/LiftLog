8dc76dc78ad329a97cdcd8c42a2fe75b6fccaf24
alexcjwei <alexcj.wei@gmail.com>
2024-09-16 20:56:26 -0700

    removes header from log index

--- LiftLog/app/(tabs)/logs/_layout.tsx
+++ LiftLog/app/(tabs)/logs/_layout.tsx
@@ -3,7 +3,12 @@ import { Stack } from 'expo-router/stack';
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name='index' />
+      <Stack.Screen
+        name='index'
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 }
